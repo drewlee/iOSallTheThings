2d737174cb58b82c2d3010db08f9c46bdb5d6d07
Andrew Lee <starsthatfell@gmail.com>
2014-06-01 15:40:17 -0700

    Updates to add prepareForSegue.

--- 04_NavigationController/NavigationController/ViewController.m
+++ 04_NavigationController/NavigationController/ViewController.m
@@ -7,11 +7,22 @@
 //
 
 #import "ViewController.h"
+#import "OtherViewController.h"
 
 @interface ViewController ()
 
 @end
 
 @implementation ViewController
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    NSString *isOtherViewController = [segue.destinationViewController isKindOfClass:[OtherViewController class]]
+        ? @"Is OtherViewController"
+        : @"Is not OtherViewController";
+
+    NSLog(@"Identifier: %@", segue.identifier);
+    NSLog(@"%@", isOtherViewController);
+}
 
 @end
